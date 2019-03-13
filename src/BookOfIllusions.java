2fd34a8f8f2fb09c0a7857c79aed351e123b3b35
ilabstaff <ilabstaff@172.16.116.67>
2019-03-12 19:25:00 -0700

    BookOfIllusions.java
    Random-image-15.jpg

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -8,8 +8,10 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
@@ -20,33 +22,80 @@ public class BookOfIllusions extends MouseAdapter {
 	 * Here we are instantiating our BookOfIllusions class and calling it’s createBook() method. This is because we want to get out of the
 	 * static main method, so that we can add a click listener to each illusion.
 	 */
-
+	JLabel label;
 	public static void main(String[] args) throws Exception {
 		BookOfIllusions illusions = new BookOfIllusions();
 		illusions.createBook();
 	}
 
-	// 1. Make a JFrame variable and initialize it using "new JFrame()"
+		// 1. Make a JFrame variable and initialize it using "new JFrame()"
 	
-	new JFrame()
+		JFrame frame = new JFrame();
 
-	private void createBook() {
+		private void createBook() {
+			
 		// 2. make the frame visible
+		
+		frame.setVisible(true);
+		
 		// 3. set the size of the frame
+		
+		frame.setSize(1000, 1000);
+		
 		// 4. find 2 images and save them to your project’s default package
+		
+		
+		
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		
+		
+		
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		
+		 label = loadImageFromComputer("Random-Photoshopped-Pictures-006.jpg");
+		
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		
+		
+		
 		// 8. add your JLabel to the frame
+		
+		frame.add(label);
+		
 		// 9. call the pack() method on the frame
+		
+		frame.pack();
+		
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		
+		frame.addMouseListener(this);
+		
 	}
 
-	public void mousePressed(MouseEvent e) {
+		private Object Image(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+		public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		
+		System.out.println("clicked!");
+		
 		// 12. remove everything from the frame that was added earlier
+		
+		frame.remove(label);
+		
 		// 13. load a new image like before (this is more than one line of code)
+		
+		label = loadImageFromComputer("Random-image-15.jpg");
+		
+		frame.add(label);
+		
 		// 14. pack the frame
+		
+		frame.pack();
+		
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups

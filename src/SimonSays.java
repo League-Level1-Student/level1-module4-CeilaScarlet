a39609d765d6b1ed78364650c2442c5e6d95ec3f
ilabstaff <ilabstaff@172.16.116.81>
2019-03-26 19:23:51 -0700

    SimonSays.java - src

--- src/SimonSays.java
+++ src/SimonSays.java
@@ -28,7 +28,7 @@ public class SimonSays extends KeyAdapter {
 	private int tries = 0;
 	private boolean simonSays = false;
 	Date timeAtStart;
-	JFrame jframe = new JFrame();
+	JFrame jframe;
 	private int score = 0; 
 	private JLabel scoreBox = new JLabel("0");
 
@@ -59,11 +59,13 @@ public class SimonSays extends KeyAdapter {
 
 		// 16. If the keyCode matches the imageIndex and "Simon says"
 		
-		if (e.getKeyCode()==imageIndex) {
+		if (e.getKeyCode()==imageIndex && simonSays == true) {
 			
 			++score;
 			
 			speak("You are correct!");
+			
+			
 			
 		}
 		
@@ -73,17 +75,51 @@ public class SimonSays extends KeyAdapter {
 		
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't say..."
 		
-		else {
+		else if (e.getKeyCode()!= imageIndex && simonSays == false){
+			
+			++score;
+			
+			speak("You are incorrect!");
 			
 			
+			
+			
+			
+			
+			
+				
 			
 		}
 		
+		++tries;
+		
+		jframe.dispose();
+		
+		showImage();
+		
+		
+		if (tries >= 3) {
+					
+					jframe.dispose();
+					
+					speak("This is your score : " + score);
+					
+					JOptionPane.showMessageDialog(null, "This is your score : " + score);
+					
+					
+					
+				}
 		// 20.  Increase the value of score
 		
 		// 21. Use the speak method to tell the user they were correct
 		
 		// 22. Increment tries by 1
+		
+		// 23. Dispose of the frame
+		
+		
+
+		// 24. Call the showImage method to show a new image
 		
 		// 25. If tries is greater than 9 (or however many you want)...
 		
@@ -91,39 +127,36 @@ public class SimonSays extends KeyAdapter {
 		
 		// 27. Exit the program
 
-		// 23. Dispose of the frame
-
-		// 24. Call the showImage method to show a new image
 	}
 
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
 		
-		JFrame frame = new JFrame();
+		jframe = new JFrame();
 		
 		// 6. Set the frame to visible
 		
-		frame.setVisible(true);
+		jframe.setVisible(true);
 
 		// 7. Uncomment the following line to add a random image to your frame
 		
-		frame.add(getNextRandomImage());
+		jframe.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
 		
-		frame.setName("Larry");
+		jframe.setName("Larry");
 
 		// 9. Pack the frame
 		
-		frame.pack();
+		jframe.pack();
 		
 		// 10. Set the defaultCloseOperation of your from to JFrame.EXIT_ON_CLOSE
 		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		// 11. Add a key listener to the frame
 		
-		frame.addKeyListener(this);
+		jframe.addKeyListener(this);
 
 		// 12. Create a new instance of Random
 		
@@ -135,10 +168,14 @@ public class SimonSays extends KeyAdapter {
 			
 			speak("Simon says press this key");
 			
+			simonSays = true;
+			
 		}
 		else {
 			
 			speak("Press this key");
+			
+			simonSays = false;
 		}
 
 		// 14. Above, set the value of simonSays to true/false appropriately

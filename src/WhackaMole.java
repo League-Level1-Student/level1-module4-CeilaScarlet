e24a89782a0cd5b5ab61a83fb53e6deb10b9d83a
ilabstaff <ilabstaff@172.16.116.81>
2019-05-21 19:15:25 -0700

    WhackaMole

--- src/WhackaMole.java
+++ src/WhackaMole.java
@@ -7,12 +7,20 @@ import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import com.sun.media.jfxmedia.AudioClip;
 
 public class WhackaMole implements ActionListener{
 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	private int molescore = 0;
+	private AudioClip sound;
+	private int molewhacked = 0;
+	private int molemissed = 0;
+	Date timeAtStart;
 		
 	WhackaMole() {
 		
@@ -27,15 +35,19 @@ public class WhackaMole implements ActionListener{
 		for (int i = 0; i < 24; i++) {
 			
 		if (i == ran) {
+			
 			JButton button = new JButton("Mole");	
 			panel.add(button);	
 			button.addActionListener(this);
+			
 		}	
 		
 		else {
+			
 		JButton button = new JButton();	
 		panel.add(button);	
 		button.addActionListener(this);
+		
 		}
 		
 		
@@ -49,13 +61,13 @@ public class WhackaMole implements ActionListener{
 		
 	public static void main(String[] args) {
 		
-		WhackaMole Wam = new WhackaMole();
+		
 		
 	}
 	
 	private void drawButtons(Random button){
 		
-
+		WhackaMole Wam = new WhackaMole();
 		
 	}
 	
@@ -66,14 +78,56 @@ public class WhackaMole implements ActionListener{
 			
 			JButton buttonPressed = (JButton) e.getSource();
 			
-		
-				
+			if (molewhacked == 10) {
+				endGame(timeAtStart,molewhacked);
+			}
+			
+			
+		if (buttonPressed.equals("Mole")) {
+			
+			molewhacked++;
+			molescore++;
+			frame.dispose();
+			drawButtons();
+			
+			
+			
+		}
+		else {
+			
+			speak("You missed!!!");
+			molemissed--;
+			frame.dispose();
+			drawButtons();
+		}		
 			
 			
 			
 		}
 
-	
-	
+		private void drawButtons() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		void speak(String words) {
+		     try {
+		          Runtime.getRuntime().exec("say " + words).waitFor();
+		     } catch (Exception e) {
+		          e.printStackTrace();
+		     }
+		}
+
+		private void endGame(Date timeAtStart, int molesWhacked) {
+		     Date timeAtEnd = new Date();
+		     JOptionPane.showMessageDialog(null, "Your whack rate is "
+		          + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+		          + " moles per second.");
+		}
+
+		private void playSound(String fileName) {
+		     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		     sound.play();
+		}
 	
 }

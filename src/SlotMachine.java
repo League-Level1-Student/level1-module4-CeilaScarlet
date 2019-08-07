554609ddd6f074741ada4fe862012a335a38a052
ilabstaff <ilabstaff@172.16.116.71>
2019-08-06 19:20:36 -0700

    SlotMachine.java - src

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,57 +1,47 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class SlotMachine {
+public class SlotMachine implements ActionListener{
 	
 	
 	JFrame jframe = new JFrame();
 	JLabel jlabel = new JLabel();
 	JPanel jpanel = new JPanel();
-	
+	JButton jbutton = new JButton();
+	JLabel cherry = new JLabel();
+	JLabel lime = new JLabel();
+	JLabel orange = new JLabel();
 	
 	SlotMachine() throws MalformedURLException{
 		
 		jframe.setVisible(true);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jbutton.setText("SPIN");
+		jbutton.addActionListener(this);
+		jpanel.add(jbutton);
 		jframe.add(jlabel);
 		jframe.add(jpanel);
 		jframe.pack();
+		cherry = createLabelImage("cher.jpg");
+		lime = createLabelImage("lim.jpg");
+		orange = createLabelImage("oran.jpg");
 		
-		jpanel.add(createLabelImage("cher.jpg"));
-		jpanel.add(createLabelImage("lim.jpg"));
-		jpanel.add(createLabelImage("oran.jpg"));
+		jpanel.add(cherry);
+		jpanel.add(lime);
+		jpanel.add(orange);
 		jframe.setSize(1900, 600);
 		
-		Random random = new Random();
 		
-		for (int i = 0; i < 3; i++) {
-			int ran = random.nextInt(2);
-			
-			if (ran == 0) {
-				
-				jpanel.add(createLabelImage("cher.jpg"));
-				
-			}
-			
-			else if (ran == 1) {
-				
-				jpanel.add(createLabelImage("lim.jpg"));
-				
-			}
-			
-			else if (ran == 2) {
-				
-				jpanel.add(createLabelImage("oran.jpg"));
-				
-			}
-		}
 		
 		
 		
@@ -66,7 +56,7 @@ public class SlotMachine {
 	}
 	
 	
-	private JLabel createLabelImage(String fileName) throws MalformedURLException{
+	private JLabel createLabelImage(String fileName){
         URL imageURL = getClass().getResource(fileName);
 	if (imageURL == null){
 		System.err.println("Could not find image " + fileName);
@@ -78,4 +68,59 @@ public class SlotMachine {
 	
 	
 }
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		Random random = new Random();
+		
+		
+		
+		JButton buttonPressed = (JButton) e.getSource();
+		
+		if (buttonPressed == jbutton) {
+			
+	
+			
+			jpanel.removeAll();
+			jpanel.add(jbutton);
+			
+			
+			for (int i = 0; i < 3; i++) {
+				
+				int ran = random.nextInt(3);
+				
+				if (ran == 0) {
+					
+					
+					
+					jpanel.add(createLabelImage("cher.jpg"));
+					
+				}
+				
+				else if (ran == 1) {
+					
+					
+					
+					jpanel.add(createLabelImage("lim.jpg"));
+					
+				}
+				
+				else if (ran == 2) {
+					
+					jpanel.add(createLabelImage("oran.jpg"));
+					
+				}
+			}
+			
+			
+			
+		}
+	
+		
+		jframe.pack();
+		
+	}
 }

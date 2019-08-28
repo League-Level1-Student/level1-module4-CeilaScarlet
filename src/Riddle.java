6c26a603a36b213c76b3a1f3d4d3331412cbb2d0
ilabstaff <ilabstaff@172.16.116.68>
2019-08-27 19:25:08 -0700

    Riddle

---
+++ src/Riddle.java
@@ -0,0 +1,33 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Riddle {
+	
+	JFrame jframe = new JFrame();
+	JLabel jlabel = new JLabel();
+	JButton obutton = new JButton();
+	JButton tbutton = new JButton();
+	JTextField jtf = new JTextField();
+	
+	
+	Riddle(){
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+public static void main(String[] args) {
+	
+	
+	
+	
+}
+}

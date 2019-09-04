d3792a99907c211ba0ef2701dcfd7e90573b62ae
ilabstaff <ilabstaff@172.16.116.128>
2019-09-03 19:19:39 -0700

    Riddle

--- src/Riddle.java
+++ src/Riddle.java
@@ -1,24 +1,51 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-public class Riddle {
+public class Riddle implements ActionListener {
 	
 	JFrame jframe = new JFrame();
 	JLabel jlabel = new JLabel();
 	JButton obutton = new JButton();
 	JButton tbutton = new JButton();
-	JTextField jtf = new JTextField();
+	JTextField aple = new JTextField();
+	String Answer = "g";
+	
+	//Write a program that displays a JFrame to the user. 
+	//The JFrame will have a JLabel, two JButtons, and a JTextField. The JLabel will provide a riddle to the user.
+	//The user will type his answer in the JTextField and click the submit button. 
+	//When the submit button is clicked, let the user know if the answer they typed in the JTextField is correct or not. 
+	//The other JButton will be a hint button. 
+	//Display a clue to the riddle when this button is clicked.
 	
 	
 	Riddle(){
+	jframe.setVisible(true);
+	jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	jframe.setSize(500, 500);
+	
+	obutton.addActionListener(this);
+	tbutton.addActionListener(this);
+
+	obutton.setText("Hint");
+	tbutton.setText("Submit");
+	jlabel.add(aple);
+	jlabel.add(obutton);
+	jlabel.add(tbutton);
+	
+	jframe.add(jlabel);
+	
+	
+	
+	jframe.pack();	
 		
-		
-		
-		
-		
-		
+	
+	
 	}
 	
 	
@@ -26,8 +53,40 @@ public class Riddle {
 	
 public static void main(String[] args) {
 	
+	Riddle ri = new Riddle();
 	
 	
+}
+
+
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
 	
+	JButton buttonPressed = (JButton) e.getSource();
+	
+	if (buttonPressed == obutton) {
+		
+		JOptionPane.showMessageDialog(null, "ENDS EVERYTHING");
+		
+	}
+	
+	if (buttonPressed == tbutton) {
+		
+		if (aple.equals(Answer)) {
+			
+			JOptionPane.showMessageDialog(null, "Correct! The answer was " + Answer + "!!!");
+			
+			
+			
+		}
+		else {
+				JOptionPane.showMessageDialog(null, "Sorry but you're incorrect! The answer was " + Answer + "!!!");
+			}
+		
+		
+	}
 }
 }

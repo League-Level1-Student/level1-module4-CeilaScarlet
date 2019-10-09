31ca5ba8461afcc27c5978588249cb0bb2759a86
ilabstaff <ilabstaff@172.16.116.77>
2019-10-08 19:30:18 -0700

    TXT.rtf - lvl1 test2.rtfd

--- src/Test.java
+++ src/Test.java
@@ -5,8 +5,32 @@ public class Test {
 	public static void main(String[] args) {
 		
 		
-	Random random = new Random();	
+	cleanDishes(1);
+	setNumDishToWash(10);	
+		
+	}
+static boolean cleanDishes(int washTimeinminutes) {
+	
+	if (washTimeinminutes == 0-3) {
+	
+			
+	return false;	
+	
+	}
+	
+	return true;
+}
+
+static void setNumDishToWash(int numberofDishes) {
+	
+	if (boolean.class.equals(false)) {
+		System.out.println("not good do better");
+	}
+	else {
+		
+		System.out.println("good job!");		
 		
 		
 	}
+}
 }

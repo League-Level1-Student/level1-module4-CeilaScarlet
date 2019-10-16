5d6d0288a8140921b301b7b953335160cf240fd2
alfredopadilla <34401896+alfredopadilla@users.noreply.github.com>
2019-10-15 19:20:51 -0700

    Dishwasher.java - src

---
+++ src/Dishwasher.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class Dishwasher  {
+
+Dishwasher(){
+	
+		
+}
+
+public static void main(String[] args) {
+		
+	Dishwasher dw = new Dishwasher();
+	
+	dw.cleanDishes(1);
+    dw.setNumDishToWash(10);	
+		
+	}
+
+public static boolean cleanDishes(int washTimeinminutes) {
+	
+	if (washTimeinminutes <= 3) {
+	
+			
+	return false;	
+	
+	}
+	else {
+		
+	return true;	
+	
+	}
+	
+}
+
+public static void setNumDishToWash(int numberofDishes) {
+	
+
+if (washTimeinminutes.equals(false)) {
+		System.out.println("not good do better");
+	}
+	else {
+		
+		System.out.println("good job!");		
+		
+}
+}
+}
